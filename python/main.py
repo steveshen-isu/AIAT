7ced9977976ec19951d97db624db06a099678ab8
steveshen-isu <steveszren@gmail.com>
2025-01-01 17:40:14 +0800

    update path

--- python/main.py
+++ python/main.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 # Path to the plot.py file in the tmp folder
-script_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../tmp/plot.py'))
+script_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'tmp/plot.py'))
 
 # Execute plot.py silently
 subprocess.run(['python3', script_path], check=True)
